fa0840cd9fa91c3d622adff1cc3862002542a358
Alexandre <prudenteale1@gmail.com>
2022-01-04 20:20:30 -0300

    criando o footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
+    <div>
     <div className="App">
       <header>
        
       </header>
+    </div>
+    <Footer />
     </div>
   );
 }
