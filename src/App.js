194b06cdea0666fa872cc959118b6ab026fd0838
Pedro Junior <40725275+Pedro-jds@users.noreply.github.com>
2022-01-05 09:12:42 -0300

    pequenos ajustes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Suporte from './Components/Suporte/Suporte';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <header>
        
       </header>
+      <Suporte/>
     </div>
   );
 }
---
+++ src/Components/Suporte/Suporte.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './Suporte.module.css'
+
+const Suporte = () => {
+    return 
+}
+
+export default Suporte
