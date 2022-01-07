9cfb54fd0439d13ade8d0ad75171875415cf3dfa
patricia andrade <patricia.andrade094@gmail.com>
2022-01-06 23:11:24 -0300

    implementação loading

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import { useEffect, useState } from "react";
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
 import Home from './Components/Home/Home'
-import Login from './Components/Login/Login'
 import Suporte from './Components/Suporte/Suporte'
 import Dashboard from './Components/Dashboard/Dashboard'
-
+import Loading from "./Components/Loading/Loading";
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(()=>{
+      setLoading(false);
+    },2000)
+  }, [])
+
   return (
+    loading? <Loading/> :
     <div className="App">
       <Router>
         <Header />
